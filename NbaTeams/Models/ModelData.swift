fa6d4a1254f33c8b9d27625c707814832ef7bc42
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-02-10 08:03:49 +0000

    Minor change to formatting, and updated URL

--- NbaTeams/Models/ModelData.swift
+++ NbaTeams/Models/ModelData.swift
@@ -42,7 +42,7 @@ func loadNbaScoreboardData() -> Scoreboard {
     formatter.dateFormat = "MM/dd/yyyy"
     let formattedDate = formatter.string(from: Date())
     
-    let url = URL(string: "https://stats.nba.com/stats/scoreboard/?GameDate=\(formattedDate)&LeagueID=00&DayOffset=-100")
+    let url = URL(string: "https://stats.nba.com/stats/scoreboard/?GameDate=\(formattedDate)&LeagueID=00&DayOffset=0")
     var request = URLRequest(url: url!)
     request.httpMethod = "GET"
     request.setValue("stats.nba.com", forHTTPHeaderField: "host")
@@ -50,23 +50,17 @@ func loadNbaScoreboardData() -> Scoreboard {
     request.setValue("stats", forHTTPHeaderField: "x-nba-stats-origin")
     request.setValue("x-nba-stats-origin", forHTTPHeaderField: "Referer")
     
-    var retData1: Scoreboard = Scoreboard(resultSets: [ResultSet]())
+    var retData: Scoreboard = Scoreboard(resultSets: [ResultSet]())
     
     URLSession.shared.dataTask(with: request) { data, response, error in
         guard let data = data else { return }
-        //let str = String(decoding: data, as: UTF8.self)
-        //var d = Data(contentsOf: data)
-        
-        let retData: Scoreboard
         do {
             retData = try (JSONDecoder().decode(Scoreboard.self, from: data))
-            //retData = decodedData
-            retData1 = retData
         } catch {
             print("Error loading JSON: \(error.localizedDescription)")
         }
     }.resume()
     
-    return retData1
+    return retData
 }
 
